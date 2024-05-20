88c0955ebb2a912bce170497885027d1b7093ce3
Arne Pannemans <arne.pannemans@ml6.eu>
2024-05-20 22:04:42 +0200

    Add default route

--- backend/routes.js
+++ backend/routes.js
@@ -5,6 +5,10 @@ import { getConfig, retrieveImage, uploadImage } from './controller.js';
 const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage() });
 
+router.get('/', (req, res) => {
+    res.send('Welcome to the hidden-future project');
+});
+
 router.get('/config', getConfig);
 router.post('/retrieve', retrieveImage);
 router.post('/upload', upload.single('image'), uploadImage);
